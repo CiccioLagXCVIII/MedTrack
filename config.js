aabeeed11d75889230ea8b6715b9e0c4a3daf9c8
CiccioLagXCVIII <francescolv78@gmail.com>
2026-04-14 18:08:17 +0200

    Aggiunti Orari In Liberi E Refactoring Codice JS

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 const CONFIG = {
-    SUPABASE_URL: 'MY_SUPABASE_URL',
-    SUPABASE_KEY: 'MY_SUPABASE_KEY'
+    SUPABASE_URL: 'LA_TUA_URL',
+    SUPABASE_KEY: 'LA_TUA_KEY',
+
+    // ID Dell'Admin Per La Dev Mode
+    ADMIN_UID: 'INCOLLA-QUI-IL-TUO-UID',
+
+    // Mappatura ID -> Nome
+    // Se Un ID Non Si Trova Qui, L'App Scriverà "Sconosciuto"
+    TEAM_NAMES: {
+        'INCOLLA-QUI-IL-TUO-UID': 'Admin',
+        'INCOLLA-QUI-L-ID-DEL-COLLEGA-1': 'Marco',
+        'INCOLLA-QUI-L-ID-DEL-COLLEGA-2': 'Giulia'
+    }
 };
 
 const sb = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
